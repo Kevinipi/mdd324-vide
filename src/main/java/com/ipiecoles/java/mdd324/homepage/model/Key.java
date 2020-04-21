e11fc76d3a2c9af9a0940189fc4e657c41be1b57
Kevinipi <kevin.pose@ipilyon.net>
2020-04-21 13:26:49 +0200

    Create class key in model

---
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/model/Key.java
@@ -0,0 +1,20 @@
+package com.ipiecoles.java.mdd324.homepage.model;
+
+public class Key {
+    private String key;
+
+    public Key(String key) {
+        this.key = key;
+    }
+
+    public Key() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
